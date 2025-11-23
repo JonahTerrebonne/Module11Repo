67a939ea16b1004b74ad3e71248696c1d9e2a07b
JonahTerrebonne <jonah.tbone@gmail.com>
2025-11-23 18:16:03 -0500

    Added new message

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@
 # 11/23/2025
 # A simple Python program to print a message
 
+# Jonah Terrebonne
+# 11/23/2025 (later in the day)
+# added new message
+
 print("Hello, World!")
+
+print("Welcome to GitHub!")
